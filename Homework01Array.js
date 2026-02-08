5c1eeb2325a8244ae46ba51af15a375d2d86569e
Juan Jose Bahamon <juan.bahamon@uao.edu.co>
2026-02-07 20:19:13 -0500

    Primer commit

---
+++ Homework01Array.js
@@ -0,0 +1,8 @@
+// Array de usuarios
+
+const usuarios = [{ id: 1, nombre: "Andres", edad: 22 ,activo: true}, 
+    { id: 2, nombre: "Maria", edad: 30 ,activo: false},
+    { id: 3, nombre: "Juan", edad: 18 ,activo: true},
+    { id: 4, nombre: "Ana", edad: 25 ,activo: false},
+    { id: 5, nombre: "Luis", edad: 28 ,activo: true}
+]
