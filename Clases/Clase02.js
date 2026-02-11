86740aa912e7c889720b6a3a738e9c4a05d666b9
JUAN JOSE BAHAMON LOZANO <juan.bahamon@uao.edu.co>
2026-02-11 14:35:41 -0500

    Commit clase 11/02/26

---
+++ Clases/Clase02.js
@@ -0,0 +1,8 @@
+// Destructuring Assignement (Nos permite desempacar valores que tenemos en un array, o las propiedades de un objeto en variables distintas)
+const [a, b, c] = [10, 20, 30]
+
+const {name: university, city} = { name: "Autonoma", city: "Cali"}
+
+console.log(a + b)
+console.log(university)
+
