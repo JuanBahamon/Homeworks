20f5a7c1c507e38c9766c7a7e0147297f384f913
JUAN JOSE BAHAMON LOZANO <juan.bahamon@uao.edu.co>
2026-02-25 15:53:01 -0500

    Primer commit homework react

---
+++ proyecto-vite-bahamon/src/components/FormularioContacto.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './FormularioContacto.css';
+
+function FormularioContacto({ alAgregarContacto }) {
+    const [nombre, setNombre] = useState('');
+    const [telefono, setTelefono] = useState('');
+}
+
+const manejarEnvio = (e) => {
+    e.preventDefault();
+    if(!nombre.trim() || !telefono.trim()) {
+        alert('Por favor, completa ambos campos.');
+        return;
+    }
+
+    alAgregarContacto(nombre, telefono);
+
+    setNombre('');
+    setTelefono('');
+};
+
+return (
+    <div className="formulario-contacto">
+      <h2>Agregar Nuevo Contacto</h2>
+      <form onSubmit={manejarEnvio}>
+        <div className="grupo-formulario">
+          <input
+            type="text"
+            placeholder="Nombre completo"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            className="input-formulario"
+          />
+        </div>
+        <div className="grupo-formulario">
+          <input
+            type="tel"
+            placeholder="Teléfono"
+            value={telefono}
+            onChange={(e) => setTelefono(e.target.value)}
+            className="input-formulario"
+          />
+        </div>
+        <button type="submit" className="boton-agregar">
+          Agregar Contacto
+        </button>
+      </form>
+    </div>
+  );
+
+
+export default FormularioContacto;
